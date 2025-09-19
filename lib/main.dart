c205d83bc85af97ba4708ece7842e7450a23b8fc
Jay <weeihua1003@gmail.com>
2025-09-19 21:32:40 +0800

    example from slick_slides

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:slick_slides/slick_slides.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SlickSlides.initialize();
+
   runApp(const MyApp());
 }
+
+const _defaultTransition = SlickFadeTransition(
+  color: Colors.black,
+);
+
+const _codeExampleA = '''class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}''';
+
+const _codeExampleB = '''class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideDeck(
+      slides: [
+        Slide(
+          builder: (context) {
+            return const TitleSlide(
+              title: Text('Slick Slides'),
+              subtitle: Text('Stunning presentations in Flutter'),
+            );
+          },
+        ),
+      ],
+    );
+  }
+}''';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,112 +48,103 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Slick Slides Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+    return SlideDeck(
+      slides: [
+        FullScreenImageSlide(
+          image: const AssetImage('assets/logo-background.jpg'),
+          title: 'Slick Slides',
+          subtitle: 'Stunning presentations\nwith Flutter',
+          alignment: const Alignment(0.6, 0.0),
+          theme: const SlideThemeData.darkAlt(),
+          transition: _defaultTransition,
+        ),
+        BulletsSlide(
+          title: 'What is Slick Slides?',
+          bulletByBullet: true,
+          bullets: const [
+            'Slick Slides was born out of the need to make nice looking '
+                'slides for Serverpod at the FlutterCon conference.',
+            'It comes with many built-in slide types, and is easy to '
+                'extend with your own.',
+            'Browse through the slides in this presentation to see '
+                'what it can do.',
+            'If you use Slick Slides for your presentation, please '
+                'give some credit to Serverpod for the work we put into '
+                'this package. Also, check out Serverpod if you haven\'t '
+                'already, it\'s a great way to build your backend in '
+                'Dart.',
+          ],
+          transition: _defaultTransition,
+        ),
+        BulletsSlide(
+          title: 'Bullets with images',
+          image: const AssetImage('assets/serverpod-avatars.webp'),
+          bullets: [
+            'Add images to you presentation with a single line of code.',
+            'Bullet point slides can have images too.',
+          ],
+          transition: _defaultTransition,
+        ),
+        PersonSlide(
+          title: 'Rockstar Flutter Developer',
+          name: 'Philippa Flutterista',
+          image: const AssetImage(
+            'assets/portrait.jpg',
+          ),
+          transition: _defaultTransition,
+        ),
+        Slide(
+          builder: (context) {
+            return const ContentLayout(
+              content: Center(
+                child: SizedBox(
+                  width: 600,
+                ),
+              ),
+            );
+          },
+          transition: const SlickFadeTransition(),
+          onPrecache: (context) async {
+          },
+        ),
+        BulletsSlide(
+          theme: const SlideThemeData.light(),
+          title: 'Themes',
+          bullets: const [
+            'Use the built in themes or create your own.',
+            'This is the default light theme.',
+          ],
+          transition: _defaultTransition,
+        ),
+        AnimatedCodeSlide(
+          formattedCode: [
+            FormattedCode(
+              code: _codeExampleA,
+            ),
+            FormattedCode(
+              code: _codeExampleB,
+              highlightedLines: [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ],
     );
   }
 }
